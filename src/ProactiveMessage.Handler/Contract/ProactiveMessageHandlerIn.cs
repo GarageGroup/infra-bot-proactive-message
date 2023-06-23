1d0be1d1c99999b51bf1f34a50dde7b7337e41cc
pmosk <pm@ggroupp.ru>
2023-06-23 14:32:53 +0400

    Use Newtonsoft.Json for ProactiveMessage

--- src/ProactiveMessage.Handler/Contract/ProactiveMessageHandlerIn.cs
+++ src/ProactiveMessage.Handler/Contract/ProactiveMessageHandlerIn.cs
@@ -1,7 +1,9 @@
+using System.Text.Json.Serialization;
 using Microsoft.Bot.Schema;
 
 namespace GarageGroup.Infra.Bot.Builder;
 
+[JsonConverter(typeof(ProactiveMessageJsonConverter))]
 public sealed record class ProactiveMessageHandlerIn
 {
     public Activity? Activity { get; init; }
